5e4382613d51a4eaab09b9377c1c3154be87ad98
Vitalii Tonchak <vitalii.tonchak@soprasteria.com>
2023-05-22 12:43:53 +0200

    initiated new workspace

---
+++ NGAPI/NGAPI/NGAPI.h
@@ -0,0 +1,18 @@
+//
+//  NGAPI.h
+//  NGAPI
+//
+//  Created by vtonchak on 22/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NGAPI.
+FOUNDATION_EXPORT double NGAPIVersionNumber;
+
+//! Project version string for NGAPI.
+FOUNDATION_EXPORT const unsigned char NGAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NGAPI/PublicHeader.h>
+
+
